fa183872abe194f0bfbe21c58ef0daac485d672d
zaater <almogtbanmr123@gmail.com>
2025-11-21 16:01:13 +0300

    switch from chan wait to wg "waitgtoup"

--- cmd/main.go
+++ cmd/main.go
@@ -3,40 +3,42 @@ package main
 import (
 	"fmt"
 	"os"
+	"sync"
 )
 
 const (
 	frame = 60
 	scale = 10
-	h = 9 * scale
-	w = 16 * scale
+	h     = 9 * scale
+	w     = 16 * scale
 )
 
 func main() {
-	ch := make(chan struct{}, frame)
-	for num := range 60 {
-		go ppm(num, ch)
+	var wg sync.WaitGroup
+	for num := range frame {
+		wg.Add(1)
+		ppm(num, &wg)
 	}
-	for range frame {
-		<-ch
-	}
+
+	wg.Wait()
 }
 
-func ppm(num int, ch chan struct{}) {
+func ppm(num int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	outputf := fmt.Sprintf("output/image-%02d.ppm", num)
 	f, err := os.Create(outputf)
 	if err != nil {
 		fmt.Print(err)
-		ch <- struct{}{}
 		return
 	}
+	defer f.Close()
 	fmt.Printf("start on %02d \n", num)
 
 	fmt.Fprint(f, "P6\n")
 	fmt.Fprintf(f, "%d %d\n", w, h)
 	fmt.Fprintf(f, "255\n")
 	for y := range h {
-		for  range w {
+		for range w {
 			if ((y+num)/scale)%2 == 0 {
 				f.Write([]byte{0xff, 0x00, 0x00})
 			} else {
@@ -45,6 +47,4 @@ func ppm(num int, ch chan struct{}) {
 		}
 	}
 	fmt.Printf("Genatare %v\n", outputf)
-	f.Close()
-	ch <- struct{}{}
 }
